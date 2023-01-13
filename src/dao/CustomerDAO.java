1dccd014b3ca7450b05e407adc22cd45a0e36123
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-01-13 23:34:03 +0530

    Add CustomerDAO Abstrac method

--- src/dao/CustomerDAO.java
+++ src/dao/CustomerDAO.java
@@ -1,4 +1,24 @@
 package dao;
 
+import db.DBConnection;
+import model.CustomerDTO;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 public interface CustomerDAO {
+
+    public ArrayList<CustomerDTO> getAllCustomer() throws SQLException, ClassNotFoundException;
+
+    public boolean saveCustomer(String id, String name,String address) throws SQLException, ClassNotFoundException;
+
+    public boolean UpdateCustomer(CustomerDTO dto) throws SQLException, ClassNotFoundException;
+
+
+    public boolean existCustomer(String id) throws SQLException, ClassNotFoundException;
+
+    public boolean DeleteCustomer(String id) throws SQLException, ClassNotFoundException;
+
+    public String generateNewId() throws SQLException, ClassNotFoundException;
+
 }
