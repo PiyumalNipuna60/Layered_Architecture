dc46d3a46ea6b383d2920c2743ab14274914d268
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-01-13 23:35:00 +0530

    Add CustomerdAOImpl implements CustomerDAO

--- src/dao/CustomerDAOImpl.java
+++ src/dao/CustomerDAOImpl.java
@@ -6,7 +6,7 @@ import model.CustomerDTO;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class CustomerDAOImpl {
+public class CustomerDAOImpl implements CustomerDAO{
 
     public ArrayList<CustomerDTO> getAllCustomer() throws SQLException, ClassNotFoundException {
         Connection connection = DBConnection.getDbConnection().getConnection();
