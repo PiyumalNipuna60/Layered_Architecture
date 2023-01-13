1bd5a409b7c8fe6eee52272503fd4969dea4d817
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-01-13 23:40:21 +0530

    Add ItemDaoImpl implements ItemDAO

--- src/dao/ItemDAOImpl.java
+++ src/dao/ItemDAOImpl.java
@@ -7,7 +7,7 @@ import view.tdm.ItemTM;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class ItemDAOImpl {
+public class ItemDAOImpl implements ItemDAO{
 
     public ArrayList<ItemDTO> getAllItem() throws SQLException, ClassNotFoundException {
         Connection connection = DBConnection.getDbConnection().getConnection();
