3d1c1eabd3cee366635bde77e0a943c15a1f4dc6
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-01-17 10:06:19 +0530

    Add content

--- src/model/CustomDTO.java
+++ src/model/CustomDTO.java
@@ -1,4 +1,156 @@
 package model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class CustomDTO {
+    private String id;
+    private String name;
+    private String address;
+
+    private String code;
+    private String description;
+    private BigDecimal unitPrice;
+    private int qtyOnHand;
+
+    private String orderId;
+    private LocalDate orderDate;
+    private String customerId;
+    private String oid;
+    private String itemCode;
+    private int qty;
+
+    public CustomDTO() {
+    }
+
+    public CustomDTO(LocalDate orderDate, String customerId, String oid, String itemCode, int qty) {
+        this.orderDate = orderDate;
+        this.customerId = customerId;
+        this.oid = oid;
+        this.itemCode = itemCode;
+        this.qty = qty;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getQtyOnHand() {
+        return qtyOnHand;
+    }
+
+    public void setQtyOnHand(int qtyOnHand) {
+        this.qtyOnHand = qtyOnHand;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomDTO{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", code='" + code + '\'' +
+                ", description='" + description + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", qtyOnHand=" + qtyOnHand +
+                ", orderId='" + orderId + '\'' +
+                ", orderDate=" + orderDate +
+                ", customerId='" + customerId + '\'' +
+                ", oid='" + oid + '\'' +
+                ", itemCode='" + itemCode + '\'' +
+                ", qty=" + qty +
+                '}';
+    }
 }
