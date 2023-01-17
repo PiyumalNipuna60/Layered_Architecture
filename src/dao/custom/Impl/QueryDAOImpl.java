67f698eeaddeec53868cb662ec53ff0a1653f208
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-01-17 09:59:15 +0530

    Add CustomDTO

--- src/dao/custom/Impl/QueryDAOImpl.java
+++ src/dao/custom/Impl/QueryDAOImpl.java
@@ -9,5 +9,11 @@ import java.sql.SQLException;
 
 public class QueryDAOImpl implements QueryDAO {
     @Override
+    public void searchOrderByOrderId(String id) throws SQLException, ClassNotFoundException {
+        String sql="select Orders.oid,Orders.date,Orders.customerID,OrderDetails.itemCode,OrderDetails.qty,OrderDetails.unitPrice from Orders inner join OrderDetails on Orders.oid=OrderDetails.oid where Orders.oid=?";
+        ResultSet rst = SqlUtil.executeQuery(sql, id);
+        while (rst.next()){
 
+        }
+    }
 }
