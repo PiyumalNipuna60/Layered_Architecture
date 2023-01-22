c89914ee7e700f695982b6cd0971246f12c122ef
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-01-22 23:36:58 +0530

    add CustomerBOImpl class

--- src/controller/ManageCustomersFormController.java
+++ src/controller/ManageCustomersFormController.java
@@ -64,6 +64,8 @@ public class ManageCustomersFormController {
                 txtCustomerId.setDisable(false);
                 txtCustomerName.setDisable(false);
                 txtCustomerAddress.setDisable(false);
+
+
             }
         });
 
