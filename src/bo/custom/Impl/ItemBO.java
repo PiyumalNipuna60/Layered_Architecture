55a3acf7387f536f77ccbe60f762d02a94a855e9
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-01-27 09:57:16 +0530

    Add implement interface

--- src/bo/custom/Impl/ItemBO.java
+++ src/bo/custom/Impl/ItemBO.java
@@ -1,4 +1,21 @@
 package bo.custom.Impl;
 
+import model.ItemDTO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public interface ItemBO {
+
+    public ArrayList<ItemDTO> getAllItem() throws SQLException, ClassNotFoundException ;
+
+    public boolean deleteItem(String code) throws SQLException, ClassNotFoundException;
+
+    public boolean saveItem(ItemDTO dto) throws SQLException, ClassNotFoundException;
+
+    public boolean updateItem(ItemDTO dto) throws SQLException, ClassNotFoundException;
+
+    public boolean existItem(String code) throws SQLException, ClassNotFoundException;
+
+    public String generateNewItemCode() throws SQLException, ClassNotFoundException;
 }
--- src/bo/custom/Impl/ItemBOImpl.java
+++ src/bo/custom/Impl/ItemBOImpl.java
@@ -7,29 +7,35 @@ import model.ItemDTO;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class ItemBOImpl {
+public class ItemBOImpl implements ItemBO{
     private final ItemDAO itemDAO = new ItemDAOImpl();
 
+    @Override
     public ArrayList<ItemDTO> getAllItem() throws SQLException, ClassNotFoundException {
         return itemDAO.getAll();
     }
 
+    @Override
     public boolean deleteItem(String code) throws SQLException, ClassNotFoundException {
         return itemDAO.Delete(code);
     }
 
+    @Override
     public boolean saveItem(ItemDTO dto) throws SQLException, ClassNotFoundException {
         return itemDAO.save(dto);
     }
 
+    @Override
     public boolean updateItem(ItemDTO dto) throws SQLException, ClassNotFoundException {
         return itemDAO.Update(dto);
     }
 
+    @Override
     public boolean existItem(String code) throws SQLException, ClassNotFoundException {
         return itemDAO.exist(code);
     }
 
+    @Override
     public String generateNewItemCode() throws SQLException, ClassNotFoundException {
         return itemDAO.generateNewId();
     }
