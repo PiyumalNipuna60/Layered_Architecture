7cb0e24db6f7b8d05c73af377badfc07ec4f9a11
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-01-28 10:08:42 +0530

    Add Content

--- src/dao/custom/DAOFactory.java
+++ src/dao/custom/DAOFactory.java
@@ -1,4 +1,35 @@
 package dao.custom;
 
 public class DAOFactory {
+    private static DAOFactory daoFactory;
+
+    private DAOFactory(){
+
+    }
+
+    public static DAOFactory getDaoFactory(){
+        if (daoFactory==null){
+            daoFactory = new DAOFactory();
+        }
+        return daoFactory;
+    }
+
+    public enum DAOTypes{
+        CUSTOMER,ITEM,ORDER,ORDERDETAILS,QUERYDAO
+    }
+
+    public void getDAO(DAOTypes types){
+        switch (types){
+            case CUSTOMER:
+                return;
+            case ITEM:
+                return;
+            case ORDER:
+                return;
+            case ORDERDETAILS:
+                return;
+            case QUERYDAO:
+                return;
+        }
+    }
 }
