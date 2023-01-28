55b93e342bc727f05259c597b6c33fc0d0d6d43a
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-01-28 16:01:57 +0530

    Add content BOFactory

--- src/bo/BOFactory.java
+++ src/bo/BOFactory.java
@@ -1,4 +1,38 @@
 package bo;
 
+import bo.custom.impl.CustomerBOImpl;
+import bo.custom.impl.ItemBOImpl;
+import bo.custom.impl.PurchaseOrderBOImpl;
+
 public class BOFactory {
+    private static BOFactory boFactory;
+
+    private BOFactory() {
+
+    }
+
+    public static BOFactory getBoFactory() {
+        if (boFactory == null) {
+            boFactory = new BOFactory();
+        }
+        return boFactory;
+    }
+
+    public static Object getBO(BOType type) {
+        switch (type) {
+            case CUSTOMER:
+                return new CustomerBOImpl();
+            case ITem:
+                return new ItemBOImpl();
+            case purchase_order:
+                return new PurchaseOrderBOImpl();
+            default:
+                return null;
+        }
+    }
+
+    public enum BOType {
+        CUSTOMER, ITem, purchase_order
+    }
+
 }
